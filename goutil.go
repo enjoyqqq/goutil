78bd0453b4698665a43a4e24d2f81fd839c61465
qinzhengpeng <qinzhengpeng@didiglobal.com>
2021-06-17 20:23:49 +0800

    add FindType func

--- goutil.go
+++ goutil.go
@@ -1,9 +1,22 @@
 package goutil
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func SayHello() {
-    fmt.Println("hello qzp")
+	fmt.Println("hello qzp")
+}
+
+func FindType(i interface{}) {
+	switch x := i.(type) {
+	case int:
+		fmt.Println(x, "is int")
+	case string:
+		fmt.Println(x, "is string")
+	case nil:
+		fmt.Println(x, "is nil")
+	default:
+		fmt.Println(x, "not type matched")
+	}
 }
