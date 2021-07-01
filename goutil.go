57d71c75147e89a80471cc40ae5a327a6e6d399b
qinzhengpeng <qinzhengpeng@didiglobal.com>
2021-07-01 15:49:21 +0800

    add GetRandomWelcome

--- goutil.go
+++ goutil.go
@@ -2,6 +2,8 @@ package goutil
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func SayHello() {
@@ -19,4 +21,21 @@ func FindType(i interface{}) {
 	default:
 		fmt.Println(x, "not type matched")
 	}
+}
+
+func GetRandomWelcome() {
+	t := time.Now()
+	dateFmt := t.Format("2006-01-02 15:04:05") //go诞生的时间
+	logStr := "[当前的时间是:" + dateFmt + "]"
+
+	sWelcomeList := []string{"come on qzp", "happy everyday qzp", "take easy qzp", "you are best qzp", "relax yourself qzp"}
+	iWelcomeListLen := len(sWelcomeList)
+	// for idx := 0; idx < sWelcomeLen; idx++ {
+	// 	fmt.Println(sWelcomeList[idx])
+	// }
+
+	rand.Seed(time.Now().UnixNano())
+	iRandNum := rand.Intn(iWelcomeListLen)
+	logStr += sWelcomeList[iRandNum]
+	fmt.Println(logStr)
 }
